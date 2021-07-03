98e8497e86c696fe8564588e0b6a8c7a9369fc03
Mike Solomon <mike@meeshkan.com>
2021-07-03 14:01:03 +0300

    Better formatting

--- src/WAGS/Lib/BufferPool.purs
+++ src/WAGS/Lib/BufferPool.purs
@@ -1,7 +1,6 @@
 module BufferPool where
 
 import Prelude
-
 import Control.Comonad.Cofree (Cofree, head, tail, (:<))
 import Data.FunctorWithIndex (mapWithIndex)
 import Data.Array as A
@@ -27,28 +26,14 @@ type TimeHeadroomRate
 type BufferInternal
   = { startedAt :: Number, env :: SAPFofT }
 
-
-type Buffy = { gain :: AudioParameter, onOff :: APOnOff }
+type Buffy
+  = { gain :: AudioParameter, onOff :: APOnOff }
 
 unchangingPiecewise :: NonEmpty List (Number /\ Number)
 unchangingPiecewise = (0.0 /\ 1.0) :| Nil
-{-
--- | durAtUnitRate: at a rate of 1.0, how long until we turn on the next unit?
--- | pwf: piecewise function to apply to gain
--- | initialTime: when does this loop start in time?
-makeLoopingPool ::
-  forall n.
-  Pos n =>
-  Number ->
-  Maybe (NonEmpty List (Number /\ Number)) ->
-  Number ->
-  TimeHeadroomRate ->
-  Cofree ((->) TimeHeadroomRate) (V.Vec n Buffy)
-makeLoopingPool durAtUnitRate pwf initialTime = ?hole
--}
--- | pwf: piecewise function to apply to gain
 
-type InternalState = { bufferInternal :: Maybe BufferInternal, buffy :: Buffy }
+type InternalState
+  = { bufferInternal :: Maybe BufferInternal, buffy :: Buffy }
 
 makeOffsetsPool ::
   forall n.
@@ -61,15 +46,21 @@ makeOffsetsPool dur pwf = go 0 (V.fill (const (Nothing :: Maybe BufferInternal))
   where
   len :: Int
   len = toInt' (Proxy :: _ n)
+
   notPlaying :: { bufferInternal :: Maybe BufferInternal, buffy :: Buffy }
-  notPlaying =  { bufferInternal: Nothing, buffy: { gain: pure 0.0, onOff: pure Off } }
+  notPlaying = { bufferInternal: Nothing, buffy: { gain: pure 0.0, onOff: pure Off } }
+
   realPwf = fromMaybe unchangingPiecewise pwf
+
   refresh :: { time :: Number, headroom :: Number, offset :: Number } -> OnOff -> InternalState
   refresh { time, headroom, offset } onOff =
-      let
-        sapfot =  makePiecewise (map (over _1 (add (time + offset))) realPwf)
-        now = sapfot { time, headroom }
-      in { bufferInternal: Just { startedAt: time + offset, env: tail now }, buffy: { gain: head now, onOff: ff offset (pure onOff) } }
+    let
+      sapfot = makePiecewise (map (over _1 (add (time + offset))) realPwf)
+
+      now = sapfot { time, headroom }
+    in
+      { bufferInternal: Just { startedAt: time + offset, env: tail now }, buffy: { gain: head now, onOff: ff offset (pure onOff) } }
+
   -- cPos myPos
   maybeBufferToGainOnOff ::
     Int ->
@@ -80,13 +71,16 @@ makeOffsetsPool dur pwf = go 0 (V.fill (const (Nothing :: Maybe BufferInternal))
   maybeBufferToGainOnOff cPos { time, headroom, offsets } myPos Nothing
     | Just offset <- A.index offsets (myPos - cPos `mod` len) = refresh { time, headroom, offset } On
     | otherwise = notPlaying
+
   maybeBufferToGainOnOff cPos { time, headroom, offsets } myPos (Just { startedAt, env })
     | Just offset <- A.index offsets (myPos - cPos `mod` len) = refresh { time, headroom, offset } OffOn
     | time - startedAt > dur = notPlaying
     | otherwise =
       let
         now = env { time, headroom }
-      in { bufferInternal: Just { startedAt, env: tail now }, buffy: { gain: head now, onOff: pure On } }
+      in
+        { bufferInternal: Just { startedAt, env: tail now }, buffy: { gain: head now, onOff: pure On } }
+
   go ::
     Int ->
     V.Vec n (Maybe BufferInternal) ->
