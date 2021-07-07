8425aa75ec6de7dc162b14363c1ee52db0de5c52
Mike Solomon <mike@meeshkan.com>
2021-07-07 15:20:39 +0300

    Adds piecewise

---
+++ test/Piecewise.purs
@@ -0,0 +1,32 @@
+module Test.Piecewise where
+
+import Prelude
+import Data.List (List(..), (:))
+import Data.NonEmpty ((:|))
+import Data.Tuple.Nested ((/\))
+import Test.Spec (Spec, describe, it)
+import Test.Util (shouldEqualIsh)
+import WAGS.Graph.Parameter (ff)
+import WAGS.Lib.Piecewise (makeLoopingPiecewise, makePiecewise)
+
+testPiecewise :: Spec Unit
+testPiecewise = do
+  describe "Tests piecewise" do
+    it "Produces the correct piecewise function" do
+      let
+        pw = makePiecewise ((0.0 /\ 1.0) :| (0.3 /\ 0.75) : (0.9 /\ 0.0) : Nil)
+      pw { time: -10.0, headroom: 0.0 } `shouldEqualIsh` (pure 1.0)
+      pw { time: 0.0, headroom: 0.0 } `shouldEqualIsh` (pure 1.0)
+      pw { time: 0.1, headroom: 0.1 } `shouldEqualIsh` (pure (1.0 - (0.25 / 3.0)))
+      pw { time: 0.2, headroom: 0.05 } `shouldEqualIsh` (pure (1.0 - (0.25 * 2.0 / 3.0)))
+      pw { time: 0.2, headroom: 0.1 } `shouldEqualIsh` (ff 0.1 $ pure 0.75)
+      pw { time: 10.0, headroom: 0.0 } `shouldEqualIsh` (pure 0.0)
+  describe "Tests looping piecewise" do
+    it "Produces the correct looping function" do
+      let
+        pw = makeLoopingPiecewise ((0.0 /\ 1.0) :| (0.3 /\ 0.75) : (1.0 /\ 0.0) : Nil)
+      pw { time: 0.0, headroom: 0.0 } `shouldEqualIsh` (pure 1.0)
+      pw { time: 0.1, headroom: 0.1 } `shouldEqualIsh` (pure (1.0 - (0.25 / 3.0)))
+      pw { time: 0.2, headroom: 0.05 } `shouldEqualIsh` (pure (1.0 - (0.25 * 2.0 / 3.0)))
+      pw { time: 0.2, headroom: 0.1 } `shouldEqualIsh` (ff 0.1 $ pure 0.75)
+      pw { time: 10.1, headroom: 0.1 } `shouldEqualIsh` (pure (1.0 - (0.25 / 3.0)))
