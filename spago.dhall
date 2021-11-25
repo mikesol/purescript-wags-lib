a64d8bf6aba3b10eb3ea9b95097174d37f646aac
Mike Solomon <mike@wavr.so>
2021-11-25 11:25:56 +0200

    Tweaks tidal parser

--- spago.dhall
+++ spago.dhall
@@ -7,6 +7,7 @@
   , "console"
   , "control"
   , "css"
+  , "debug"
   , "effect"
   , "either"
   , "event"
--- test/Tidal.purs
+++ test/Tidal.purs
@@ -8,7 +8,7 @@ import Data.List.Types (List(..), NonEmptyList(..))
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
 import Data.NonEmpty ((:|))
-import Test.Spec (Spec, describe, it)
+import Test.Spec (Spec, describe, it, itOnly)
 import Test.Spec.Assertions (shouldEqual)
 import Text.Parsing.StringParser (runParser)
 import WAGS.Lib.Tidal.Cycle (noteFromSample, noteFromSample', Cycle(..))
@@ -16,27 +16,29 @@ import WAGS.Lib.Tidal.Samples as S
 import WAGS.Lib.Tidal.Tidal (c2s, cycleP_, unrest)
 import WAGS.Lib.Tidal.Types (Note(..), NoteInTime(..), _right)
 
+runWrapped = runParser cycleP_ <<< (\x -> "[" <> x <> "]")
+
 testTidal :: Spec Unit
 testTidal = do
   describe "Tests parser" do
           it "Works on simple imput" do
-            runParser cycleP_
+            runWrapped
               "tabla2:42" `shouldEqual` Right (noteFromSample S.tabla2_42__Sample)
-            runParser cycleP_
+            runWrapped
               "  tabla2:42" `shouldEqual` Right (noteFromSample S.tabla2_42__Sample)
-            runParser cycleP_
+            runWrapped
               "tabla2:42 hc:0" `shouldEqual` Right (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.tabla2_42__Sample) :| (noteFromSample S.hc_0__Sample) : Nil) })
           it "Works on internal cycles" do
-            runParser cycleP_
+            runWrapped
               "[tabla2:42 hc:0] tabla2:42" `shouldEqual` Right (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.tabla2_42__Sample) :| (noteFromSample S.hc_0__Sample) : Nil) } :| (noteFromSample S.tabla2_42__Sample) : Nil) })
           it "Works on internal cycles 2" do
-            runParser cycleP_
+            runWrapped
               "tabla2:42*2 tabla2:41" `shouldEqual` Right (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.tabla2_42__Sample) :| (noteFromSample S.tabla2_42__Sample) : Nil) } :| (noteFromSample S.tabla2_41__Sample) : Nil) })
           it "Works on branching cycles" do
-            runParser cycleP_
+            runWrapped
               "[tabla2:42 <hc:0 tech:0>] tabla2:42" `shouldEqual` Right (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.tabla2_42__Sample) :| (Branching { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.hc_0__Sample) :| (noteFromSample S.tech_0__Sample) : Nil) }) : Nil) } :| (noteFromSample S.tabla2_42__Sample) : Nil) })
           it "Works on dual groups" do
-            runParser cycleP_
+            runWrapped
               "[tabla2 tabla2] [diphone2 <sitar latibro>]" `shouldEqual` Right
               ( Internal
                   { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList
@@ -57,7 +59,7 @@ testTidal = do
                   }
               )
           it "Works on simultaneous cycles" do
-            runParser cycleP_
+            runWrapped
               "diphone2 diphone2  , tech:0 tech:2" `shouldEqual` Right
               ( Simultaneous
                   { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList
@@ -77,16 +79,16 @@ testTidal = do
                   }
               )
           it "Works on simultaneous cycles 2" do
-            runParser cycleP_
+            runWrapped
               "[tabla2:42 <hc:0 tech:0>] , tabla2:42" `shouldEqual` Right (Simultaneous { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.tabla2_42__Sample) :| (Branching { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.hc_0__Sample) :| (noteFromSample S.tech_0__Sample) : Nil) }) : Nil) } :| (noteFromSample S.tabla2_42__Sample) : Nil) })
           it "Works on weights" do
-            runParser cycleP_
+            runWrapped
               "[tabla2:42 _ <hc:0 tech:0>] , tabla2:42 _ _" `shouldEqual` Right (Simultaneous { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample' 2.0 S.tabla2_42__Sample) :| (Branching { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.hc_0__Sample) :| (noteFromSample S.tech_0__Sample) : Nil) }) : Nil) } :| (noteFromSample' 3.0 S.tabla2_42__Sample) : Nil) })
           it "Works on branching cycles with internal cycle" do
-            runParser cycleP_
+            runWrapped
               "[tabla2:42 <[hc:0 hc:0] tech:0>] tabla2:42" `shouldEqual` Right (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList (Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.tabla2_42__Sample) :| (Branching { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((Internal { env: { weight: 1.0, tag: Nothing }, nel: NonEmptyList ((noteFromSample S.hc_0__Sample) :| (noteFromSample S.hc_0__Sample) : Nil) }) :| (noteFromSample S.tech_0__Sample) : Nil) }) : Nil) } :| (noteFromSample S.tabla2_42__Sample) : Nil) })
   describe "Test cycle to sequence" do
-    let run = map (flip c2s $ wrap 1.0) <<< runParser cycleP_
+    let run = map (flip c2s $ wrap 1.0) <<< runWrapped
     it "Works on simple imput" do
       run "tabla2:42" `shouldEqual` Right (pure (NonEmptyList (NoteInTime { tag: Nothing, duration: 1.0, startsAt: 0.0, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.tabla2_42__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } :| Nil)))
       run "tabla2:42 hc:0" `shouldEqual` Right (pure (NonEmptyList (NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.0, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.tabla2_42__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } :| NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.5, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.hc_0__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : Nil)))
@@ -103,7 +105,7 @@ testTidal = do
     it "Works on simultaneous cycles" do
       run "[tabla2:42 <hc:0 ~>] , hc:0" `shouldEqual` Right (NonEmptyList ((NonEmptyList (NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.0, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.tabla2_42__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } :| NoteInTime { tag: Nothing, duration: 1.0, startsAt: 0.0, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.hc_0__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.5, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.hc_0__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : Nil)) :| (NonEmptyList (NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.0, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.tabla2_42__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } :| NoteInTime { tag: Nothing, duration: 1.0, startsAt: 0.0, cycleDuration: 1.0, note: Just $ Note { sampleFoT: _right S.hc_0__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.5, cycleDuration: 1.0, note: Nothing } : Nil)) : Nil))
   describe "Test cycle to sequence without rests" do
-    let run = map (unrest <<< flip c2s (wrap 1.0)) <<< runParser cycleP_
+    let run = map (unrest <<< flip c2s (wrap 1.0)) <<< runWrapped
     it "Works on simple imput" do
       run "tabla2:42" `shouldEqual` Right (pure ((NoteInTime { tag: Nothing, duration: 1.0, startsAt: 0.0, cycleDuration: 1.0, note: Note { sampleFoT: _right S.tabla2_42__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : Nil)))
       run "tabla2:42 hc:0" `shouldEqual` Right (pure ((NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.0, cycleDuration: 1.0, note: Note { sampleFoT: _right S.tabla2_42__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : NoteInTime { tag: Nothing, duration: 0.5, startsAt: 0.5, cycleDuration: 1.0, note: Note { sampleFoT: _right S.hc_0__Sample, rateFoT: const 1.0, forward: true, bufferOffsetFoT: const 0.0, volumeFoT: const 1.0 } } : Nil)))
