39b4b206ba0b84bf1fc260d9a94422cb0a667747
Mike Solomon <mike@meeshkan.com>
2022-02-13 09:41:16 +0200

    Renames drones

--- src/WAGS/Lib/Sounds/Drones.purs
+++ src/WAGS/Lib/Sounds/Drones.purs
@@ -7,12 +7,12 @@ import WAGS.Lib.Tidal.Types (BufferUrl(..))
 
 dronesRaw :: Object String
 dronesRaw = fromHomogeneous
-  { "spacewind:0": "https://media.graphcms.com/huYqv3YTRp2897eaReSl"
-  , "ambienta:0": "https://media.graphcms.com/dOsnH4XSHaYxut8hT7lQ"
-  , "lowdark:0": "https://media.graphcms.com/x9aA1yAgRwCCBdzYBNIj"
-  , "harmonium:0": "https://media.graphcms.com/5uiXrX2IQW2hoktWAowd"
-  , "hollowair:0": "https://media.graphcms.com/eG3qD6c5SEaTia8m5s4O"
-  , "digeridoo:0": "https://media.graphcms.com/FeqmdhiQVjqrQfpxoHVw"
+  { "spacewind": "https://media.graphcms.com/huYqv3YTRp2897eaReSl"
+  , "ambienta": "https://media.graphcms.com/dOsnH4XSHaYxut8hT7lQ"
+  , "lowdark": "https://media.graphcms.com/x9aA1yAgRwCCBdzYBNIj"
+  , "harmonium": "https://media.graphcms.com/5uiXrX2IQW2hoktWAowd"
+  , "hollowair": "https://media.graphcms.com/eG3qD6c5SEaTia8m5s4O"
+  , "digeridoo": "https://media.graphcms.com/FeqmdhiQVjqrQfpxoHVw"
   }
 
 drones :: Object BufferUrl
